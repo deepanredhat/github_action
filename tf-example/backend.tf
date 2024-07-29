f101962554c3f27b8055421de3cd65013b373ed7
deepanredhat <deepan.redhat@gmail.com>
2024-07-29 18:11:05 +0100

    changed the value

--- tf-example/backend.tf
+++ tf-example/backend.tf
@@ -5,7 +5,6 @@ terraform {
     dynamodb_table = "dynamo-terraform-state-lock"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    profile        = var.profile
 
   }
 }
