0c375e63ec97353a19c7f58a20c8fb0903aebeed
deepanredhat <deepan.redhat@gmail.com>
2024-07-29 17:16:01 +0100

    added backed file

---
+++ tf-example/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "rgas-dev-red-sun"
+    dynamodb_table = "dynamo-terraform-state-lock"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    profile        = "dev"
+
+  }
+}
--- tf-example/resource.tf
+++ tf-example/resource.tf
@@ -3,12 +3,11 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-059c3a7e108a64f3a"]
   key_name               = "terraform"
-  count                  = terraform.workspace == "default" ? 2 : 1
   root_block_device {
     volume_size = 8
   }
 
   tags = {
-    Name = format("%s_%s_%s", terraform.workspace, var.Name, count.index)
+    Name = var.Name
   }
 }
